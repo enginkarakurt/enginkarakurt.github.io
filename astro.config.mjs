22f3477daa3f43561a02d1664a617cafaff2d0c0
Engin Karakurt <155079545+enginkarakurt@users.noreply.github.com>
2025-04-29 21:25:24 +0200

    Add Inter font, update text

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,20 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://enginkarakurt.github.io",
+  experimental: {
+    fonts: [
+      {
+        provider: "local",
+        name: "Inter",
+        cssVariable: "--font-inter",
+        variants: [
+          {
+            weight: 400,
+            style: "normal",
+            src: ["./src/assets/fonts/Inter-Regular.woff2"],
+          },
+        ],
+      },
+    ],
+  },
 });
